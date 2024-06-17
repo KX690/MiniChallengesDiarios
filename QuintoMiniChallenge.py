2df8fc7217ccabe3e218e6dbcc33060209645fbd
Kevin Lezcano <kevin_lezcano@hotmail.com>
2024-06-17 13:21:57 -0400

    Quinto Mini Challenge (Diccionario)

---
+++ QuintoMiniChallenge.py
@@ -0,0 +1,10 @@
+lista1 = ["Kevin", "Hugo", "Pedro", "Juan"]
+lista2 = [4370158, 2317230,3123145,64574588]
+
+diccionario = {}
+
+for i in range(4):
+    diccionario[lista1[i]] = lista2[i]
+
+
+print(diccionario)
