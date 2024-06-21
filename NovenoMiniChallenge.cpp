514819153c84b1b101991473c75d4e710b330c63
Kevin Lezcano <kevin_lezcano@hotmail.com>
2024-06-21 12:42:20 -0400

    Noveno Mini Challenge (Suma de numeros en C++

---
+++ NovenoMiniChallenge.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int num1, num2, suma;
+
+int main(){
+
+    cout<<"Introduzca un numero: ";
+    cin>>num1;
+    cout<<"Introduzca otro numero: ";
+    cin>>num2;
+
+    suma = num1+ num2;
+
+    cout<<"La suma de los numeros es: "<<suma<<endl;
+
+
+    return 0;
+}
